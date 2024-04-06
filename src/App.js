f975d33c54aa2d21e6c1d977ac209729736a858d
wangkeben <623674037@qq.com>
2024-04-06 21:05:15 +0800

    conflict solving

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
+import NavigationBar from './components/Navbar/NavigationBar';
 import ImageGenerator from './views/T2I/ImageGenerator';
 import WaterMark from "./views/WaterMark/WaterMark";
 function App() {
   return (
       <Router>
-        <ImageGenerator/>
+      <div>
+        <ImageGenerator />
+      </div>
       </Router>
   );
 }
--- src/views/T2I/ImageGenerator.js
+++ src/views/T2I/ImageGenerator.js
@@ -177,7 +177,7 @@ function App() {
               </div> */}
           </div>
         </div>
-      </div>
+      </div>n
     </div>
   );
 }
