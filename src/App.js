6b736a613af322ba5cdb564e4d68e68b585408d5
夏季 <summer@xiajideMacBook-Pro.local>
2024-04-06 20:56:43 +0800

    导航栏_xia

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
-import T2I from './views/T2I/T2I';
-import T2I_left from './views/T2I/T2I_left';
+// import T2I from './views/T2I/T2I';
+// import T2I_left from './views/T2I/T2I_left';
 import ImageGenerator from './views/T2I/ImageGenerator';
-import WaterMark from "./views/WaterMark/WaterMark";
+// import WaterMark from "./views/WaterMark/WaterMark";
 function App() {
   return (
       <Router>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,16 +4,38 @@ import './Navbar.css';
 
 function Navbar() {
   return (
-    <div className="navcontainer">
-      <nav className="navbox">
-        <li className="nav-items" id="moren" >用户</li>
-        <li className="nav-items" >文生图</li>
-        <li className="nav-items">水印生成</li>
-        <li className="nav-items">水印检测</li>
-        <li className="nav-items">实体抽取</li>
-        <img src="列表.png" title="更多" />
+      <nav className="navbar">
+          <div className="nav-container">
+              <NavLink exact to="/" className="nav-logo">
+                  LOGO
+              </NavLink>
+              <ul className="nav-menu">
+                  <li className="nav-item">
+                      <NavLink exact to="/" activeClassName="active" className="nav-links">
+                          主页
+                      </NavLink>
+                  </li>
+                  <li className="nav-item">
+                      <NavLink to="/about" activeClassName="active" className="nav-links">
+                          关于我们
+                      </NavLink>
+                  </li>
+                  <li className="nav-item">
+                      <NavLink to="/services" activeClassName="active" className="nav-links">
+                          服务
+                      </NavLink>
+                  </li>
+                  <li className="nav-item">
+                      <NavLink to="/contact" activeClassName="active" className="nav-links">
+                          联系我们
+                      </NavLink>
+                  </li>
+              </ul>
+          </div>
       </nav>
-    </div>
+
+
   );
 }
+
 export default Navbar;
