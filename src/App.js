5e4c51d908fa0d13a5c8b4764d7ab8c98d220c7f
wangkeben <623674037@qq.com>
2024-04-07 15:51:57 +0800

    watermark修改

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
 import Navbar from './components/Navbar/Navbar';
-
-
-<<<<<<< HEAD
-
-=======
->>>>>>> f88acdaa6836ca00a690925343bd07969d4f0795
 import ImageGenerator from './views/T2I/ImageGenerator';
-// import WaterMark from "./views/WaterMark/WaterMark";
+import WaterMark from "./views/WaterMark/WaterMark";
 function App() {
   return (
       <Router>
       <div>
-<<<<<<< HEAD
         <Navbar />
-=======
-          <Navbar />
->>>>>>> f88acdaa6836ca00a690925343bd07969d4f0795
-        <ImageGenerator />
+        {<ImageGenerator />}
+        {<WaterMark/> }
       </div>
       </Router>
   );
 }
-
 export default App;
